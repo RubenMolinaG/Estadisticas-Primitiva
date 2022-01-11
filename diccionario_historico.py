472ae660bb5f79560ee075c533595f581de5bed2
Rubén Molina Galache <rubenmolinagalache@gmail.com>
2022-01-11 10:01:57 +0100

    Modificado el fichero diccionario_historico.py

--- diccionario_historico.py
+++ diccionario_historico.py
@@ -1,16 +1,5 @@
 from libreria_primitiva import *
 
-'''
-Resumen General:
-    - Los números que más han salido -> (03/38/45/47/39/41)     // Ordenados de más a menos.
-    - Los 3 números fijos que jugaremos siempre son -> (03/38/45)
-    - Combinaciones de números que jugaremos en 2022:
-        - Figura: 3/2/1, Numeros: [03 - 22 - 36 - 38 - 45 - 47]
-        - Figura: 3/2/1, Numeros: [03 - 15 - 22 - 38 - 39 - 45]
-        - Figura: 3/2/1, Numeros: [03 - 22 - 37 - 38 - 41 - 45]
-        - Figura: 4/2/0, Numeros: [03 - 36 - 38 - 39 - 45 - 47]
-'''
-
 # Creamos el diccionario con los datos generales e históricos de la Primitiva.
 diccionario_general = crear_diccionario()
 
